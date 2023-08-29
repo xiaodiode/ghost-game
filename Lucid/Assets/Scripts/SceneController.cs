bf7fc4369e1fd5528f58fb8016ce41d4e57f0c88
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-08-29 15:54:44 -0500

    3 layer scene movement works perfectly
    generalized a formula for each scene layer to move at different speeds but end at the same position (edges of camera view). still need to link with player controller so player can move to opposite side of camera view when it reaches the scene's edges

--- Lucid/Assets/Scripts/SceneController.cs
+++ Lucid/Assets/Scripts/SceneController.cs
@@ -8,7 +8,9 @@ public class SceneController : MonoBehaviour
     public Canvas[] sceneLayers;
     public PlayerController player;
     public float[] sceneXMax;
+    public int depthValue;
     private float[] sceneSpeed;
+    public bool atSceneEdge;
     private float baseSpeed = 20f;
     private float baseTime;
     // private float cameraXMax = -67.5f;
@@ -16,6 +18,8 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sceneSpeed = new float[sceneLayers.Length];
+        atSceneEdge = true;
         baseTime = sceneXMax[sceneXMax.Length-1]/baseSpeed;
         for(int i=0; i<sceneLayers.Length; i++){
             sceneSpeed[i] = sceneXMax[i]/baseTime;
@@ -29,29 +33,40 @@ public class SceneController : MonoBehaviour
     }
     private void sceneMovement(){
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        if(horizontalInput!=0 && transform.position.x>=playerXMin && transform.position.x<=playerXMax){
-            Vector3 move = new Vector3(horizontalInput, 0f, 0f)*playerSpeed*Time.deltaTime;
-            Vector3 newPosition = transform.position + move;
-            newPosition.x = Mathf.Clamp(newPosition.x, playerXMin, playerXMax);
-            
-            transform.position = newPosition;
-
-            if(transform.position.x == playerXMin || transform.position.x == playerXMax){
-                cameraOffset = transform.position - playerCamera.transform.position;
-                // Debug.Log("changed: cameraOffset: " + cameraOffset);
+        
+        for(int i=0; i<sceneLayers.Length; i++){
+            if(sceneLayers[i].GetComponent<RectTransform>().position.x > 0 &&
+                sceneLayers[i].GetComponent<RectTransform>().position.x < sceneXMax[i]){
+                    atSceneEdge = false;
             }
+        }
+        if(horizontalInput!=0){
+            for(int i=0; i<sceneLayers.Length; i++){
+                Vector2 move = new Vector2(-horizontalInput, 0f)*sceneSpeed[i]*Time.deltaTime;
+                Debug.Log("sceneSpeed: " + sceneSpeed[i]);
+                Vector2 newPosition = sceneLayers[i].GetComponent<RectTransform>().anchoredPosition + move;
+                Debug.Log("anchoredPosition for " + i + ": " + sceneLayers[i].GetComponent<RectTransform>().anchoredPosition); 
+                newPosition.x = Mathf.Clamp(newPosition.x, sceneXMax[i] + depthValue*i, depthValue*i);
+            
+                sceneLayers[i].GetComponent<RectTransform>().anchoredPosition = newPosition;
+            }
+            
 
-            updateCamera();
+            // if(transform.position.x == playerXMin || transform.position.x == playerXMax){
+            //     cameraOffset = transform.position - playerCamera.transform.position;
+            //     // Debug.Log("changed: cameraOffset: " + cameraOffset);
+            // }
+
+            // updateCamera();
         }
         
     }
-    private void updateCamera(){
-        if(playerCamera.transform.position.x >= cameraXMin && playerCamera.transform.position.x <= cameraXMax){
-            Vector3 newPosition = gameObject.transform.position - cameraOffset;
-            newPosition.x = Mathf.Clamp(newPosition.x, cameraXMin, cameraXMax);
+    // private void updateCamera(){
+    //     if(playerCamera.transform.position.x >= cameraXMin && playerCamera.transform.position.x <= cameraXMax){
+    //         Vector3 newPosition = gameObject.transform.position - cameraOffset;
+    //         newPosition.x = Mathf.Clamp(newPosition.x, cameraXMin, cameraXMax);
 
-            playerCamera.transform.position = newPosition;
-        }
-    }
+    //         playerCamera.transform.position = newPosition;
+    //     }
+    // }
 }
