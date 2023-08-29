e530db3261e4861bf0a70c086966c6f6084a9886
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-08-29 02:25:11 -0500

    player and camera movements works
    player can move right and left in the room. player starts at the left of camera view and switches to the right of camera view once they reach the rightmost of the room. room is also set up with glowing windows

--- Lucid/Assets/Scripts/PlayerController.cs
+++ Lucid/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera playerCamera;
+    private Vector3 playerPosition, cameraPosition, cameraOffset;
+    private float cameraXMax = 50;
+    private float cameraXMin = -17.5f;
+    private float playerXMax = 61.5f;
+    private float playerXMin = -29;
+    private float playerSpeed = 20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPosition = transform.position;
+        cameraPosition = playerCamera.transform.position;
+
+        cameraOffset = playerPosition - cameraPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        playerMovement();
+    }
+
+    private void playerMovement(){
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if(horizontalInput!=0 && transform.position.x>=playerXMin && transform.position.x<=playerXMax){
+            Vector3 move = new Vector3(horizontalInput, 0f, 0f)*playerSpeed*Time.deltaTime;
+            Vector3 newPosition = transform.position + move;
+            newPosition.x = Mathf.Clamp(newPosition.x, playerXMin, playerXMax);
+            
+            transform.position = newPosition;
+
+            if(transform.position.x == playerXMin || transform.position.x == playerXMax){
+                cameraOffset = transform.position - playerCamera.transform.position;
+                // Debug.Log("changed: cameraOffset: " + cameraOffset);
+            }
+
+            updateCamera();
+        }
+        
+    }
+    private void updateCamera(){
+        if(playerCamera.transform.position.x >= cameraXMin && playerCamera.transform.position.x <= cameraXMax){
+            Vector3 newPosition = gameObject.transform.position - cameraOffset;
+            newPosition.x = Mathf.Clamp(newPosition.x, cameraXMin, cameraXMax);
+
+            playerCamera.transform.position = newPosition;
+        }
     }
 }
---
+++ Lucid/Assets/Scripts/Timer.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public PlayerController player;
+    private int hours, minutes, seconds;
+    private int secondsStarted, secondsPassed;
+    private TextMeshProUGUI time;
+    private string leadingZeroH, leadingZeroM, leadingZeroS;
+    private string timeText = "00:00:00";
+    private int totalSeconds = 5;
+    bool countUp = false; bool countDown = false;
+    public bool levelClear;
+    int wasCountingUp = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelClear = false;
+        time = transform.GetComponent<TextMeshProUGUI>();
+        leadingZeroH = "0";
+        leadingZeroM = "0";
+        leadingZeroS = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(countUp){
+            // secondsPassed = Mathf.FloorToInt(Time.time) - secondsStarted;
+            secondsPassed = Mathf.FloorToInt(Time.time) - secondsStarted;
+            seconds = secondsPassed % 60;
+            minutes = (secondsPassed/60) % 60;
+            hours = (secondsPassed/3600) % 60;
+            if(hours > 9){
+                leadingZeroH = "";
+            }
+            else{
+                leadingZeroH = "0";
+            }
+            if(minutes > 9){
+                leadingZeroM = ":";
+            }
+            else{
+                leadingZeroM = ":0";
+            }
+            if(seconds > 9){
+                leadingZeroS = ":";
+            }
+            else{
+                leadingZeroS = ":0";
+            }
+            timeText = leadingZeroH + hours.ToString() + leadingZeroM + minutes.ToString() + leadingZeroS + seconds.ToString();
+            
+        }
+        else if(countDown){
+            secondsPassed = totalSeconds - (Mathf.FloorToInt(Time.time) - secondsStarted);
+            seconds = secondsPassed % 60;
+            minutes = (secondsPassed/60) % 60;
+            if(minutes > 9){
+                leadingZeroM = "";
+            }
+            else{
+                leadingZeroM = "0";
+            }
+            if(seconds > 9){
+                leadingZeroS = ":";
+            }
+            else{
+                leadingZeroS = ":0";
+            }
+            if(secondsPassed == 0){
+                levelClear = true;
+                // if(player.selectedLevel == player.levelsUnlocked){
+                //     gemController.enableGemPopup("level");
+                //     if(player.selectedLevel != 101)
+                //         player.levelsUnlocked+=1;
+                // }
+                // gameManager.gameOver();
+                resetTimer();
+            }
+            timeText = leadingZeroM + minutes.ToString() + leadingZeroS + seconds.ToString();
+            
+        }
+        
+        
+        
+        time.text = timeText;
+    }
+    public void startCountUp(){
+        secondsStarted = Mathf.FloorToInt(Time.time);
+        hours = 0; minutes = 0; seconds = 0;
+        countUp = true;
+    }
+    public void startCountDown(){
+        secondsStarted = Mathf.FloorToInt(Time.time);
+        countDown = true;
+    }
+    public void pauseTimer(){
+        
+        if(countUp){
+            wasCountingUp = 1;
+            countUp = false;
+        }
+        else if(countDown){
+            wasCountingUp = 2;
+            countDown = false;
+        }
+    }
+    public void resumeTimer(){
+        if(wasCountingUp == 1)
+            countUp = true;
+        else if(wasCountingUp == 2)
+            countDown = true;
+    }
+    public void resetTimer(){
+        countUp = false;
+        countDown = false;
+        hours = 0; minutes = 0; seconds = 0;
+    }
+    public int getTime(){
+        return Mathf.FloorToInt(Time.time);
+    }
+    public int getGameTime(){
+        return secondsPassed;
+    }
+
+    public bool trackTime(float seconds, bool timeReady){
+        int startTime = Mathf.FloorToInt(Time.time);
+        while((Mathf.FloorToInt(Time.time) - startTime) != seconds){
+            timeReady = false;
+        }
+        timeReady = true;
+        return timeReady;
+    }
+}
