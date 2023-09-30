4b57766bf5427b5853e4b2a9946f5d9b1cb54f76
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-09-30 04:21:09 -0500

    gradient circle, light intensity, and masking work
    window is fully functional. windowController controls the gradient circle rotation and the light intensity on the window based off of the rotation of a reference clock. masking also works

---
+++ Lucid/Assets/Scripts/WindowController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class WindowController : MonoBehaviour
+{
+    [SerializeField] private GameObject gradientPivot;
+    [SerializeField] private ClockController accurateClock;
+    [SerializeField] private Light2D windowLight;
+    [SerializeField] private float maxLightIntensity;
+
+    Vector3 currGradientRotation;
+    float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2 full cycles of hour hand = 1 full cycle of gradient circle
+        rotationSpeed = accurateClock.rotationSpeed/2;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateGradientRotation();
+    }
+
+    private void updateGradientRotation(){
+        currGradientRotation = gradientPivot.transform.rotation.eulerAngles;
+        currGradientRotation.z += rotationSpeed*Time.deltaTime;
+        updateWindowLight(currGradientRotation.z);
+        
+        gradientPivot.transform.rotation = Quaternion.Euler(currGradientRotation);
+    }
+
+    private void updateWindowLight(float rotation){
+        if(rotation <= 180){
+            windowLight.intensity = (rotation/180)*maxLightIntensity;
+        }
+        else{
+            windowLight.intensity = maxLightIntensity - (rotation/360)*maxLightIntensity;
+        }
+    }
+}
