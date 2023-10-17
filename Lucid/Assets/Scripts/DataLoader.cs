16a02c6d09bb1216505433a3b055425857e846fb
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-10-17 14:10:00 -0500

    created debugging function, dict loads incorrectly
    bug: both metal and gold dictionaries are filled with the very last set of comments in the metal data file for some reason. metalname key is correct

--- Lucid/Assets/Scripts/DataLoader.cs
+++ Lucid/Assets/Scripts/DataLoader.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DataLoader : MonoBehaviour
 {
@@ -105,37 +107,50 @@ public class DataLoader : MonoBehaviour
         while((fileLine = fileReader.ReadLine()) != null){
             fileLine = fileLine.Trim();
 
-            if(fileLine == "Gold" || 
-                fileLine.Contains("newMetal")){
-                    if(fileLine.Contains("newMetal")){
-                        if(comments.Count != 0){
-                            Debug.Log("goldComments: " + objectName + ", " + comments);
-                            goldComments.Add(objectName, comments);
-                            comments.Clear();
-                        }
-                        objectName = fileLine.Replace("newMetal", "").Trim();
+            if(fileLine == "Gold" || fileLine.Contains("newMetal")){
+                if(fileLine.Contains("newMetal")){
+                    if(comments.Count != 0){
+                        // foreach(string comment in comments)
+                        //     Debug.Log("goldComments: " + objectName + ", " + comment);
+                        goldComments.Add(objectName, comments);
+                        comments.Clear();
                     }
-                    else{
-                        metalComments.Add(objectName, comments);
-                        comments.Clear();  
-                    }
-                    dataSection = fileLine;
-            }
-
-            else if(dataSection == "Gold"){
-                comments.Add(fileLine);
-                Debug.Log("gold comment: " + fileLine);
-
+                    objectName = fileLine.Replace("newMetal", "").Trim();
+                }
+                else{
+                    // foreach(string comment in comments)
+                    //     Debug.Log("metalComments: " + objectName + ", " + comment);
+                    metalComments.Add(objectName, comments);
+                    comments.Clear();  
+                }
+                dataSection = fileLine;
             }
 
             else{
                 comments.Add(fileLine);
-                Debug.Log("metal comment: " + fileLine);
-                    
                 
             }
         }
 
+        debugDictionary(true, metalComments, null);
+        debugDictionary(true, goldComments, null);
+
         metalDataReady = true;
+    }
+
+    private void debugDictionary(bool valIsList, Dictionary<string, List<string>> dictWithList, Dictionary<string, string> dictWithString){
+        if(valIsList){
+            foreach(KeyValuePair<string, List<string>> pair in dictWithList){
+                foreach(string val in pair.Value){
+                    Debug.Log("val name: " + pair.Key + " | comment: " + val);
+                }
+            }
+        }
+        else{
+            foreach(KeyValuePair<string, string> pair in dictWithString){
+                Debug.Log("val name: " + pair.Key + " | comment: " + pair.Value);
+            }
+        }
+
     }
 }
