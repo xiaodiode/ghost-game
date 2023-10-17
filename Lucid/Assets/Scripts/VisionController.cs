1efa74212e49a383efe0bd4bbba5a2dee7f82c68
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-10-16 22:17:06 -0500

    fixed vision rectangle and render texture pivot
    centered both the vision rectangle and render texture (rawimage) pivots so that scaling the vision canvas can zoom accurately on whatever the cursor is pointing at

--- Lucid/Assets/Scripts/VisionController.cs
+++ Lucid/Assets/Scripts/VisionController.cs
@@ -1,31 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisionController : MonoBehaviour
 {
-    [SerializeField] private Canvas parentCanvas;
+    [SerializeField] private Canvas playerCanvas;
+    [SerializeField] private Canvas visionCanvas;
     [SerializeField] private MouseController mouse;
+
+    [Header("Zoom Settings")]
+    [SerializeField] private Camera zoomCamera;
+    [SerializeField] private float maxZoom;
+    [SerializeField] private float minZoom;
+    [SerializeField] private float zoomSpeed;
+
+    float mouseScroll;
+    float newZoom;
 
     Vector2 movePos;
     public void Start()
     {
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            parentCanvas.transform as RectTransform, Input.mousePosition,
-            parentCanvas.worldCamera,
-            out movePos);
+        // RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        //     parentCanvas.transform as RectTransform, Input.mousePosition,
+        //     parentCanvas.worldCamera,
+        //     out movePos);
+
+        minZoom = zoomCamera.fieldOfView;
     }
 
     public void Update()
     {
         if(!mouse.isLocked){
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                parentCanvas.transform as RectTransform,
-                Input.mousePosition, parentCanvas.worldCamera,
-                out movePos);
-
-            transform.position = parentCanvas.transform.TransformPoint(movePos);
+            updateVisionPosition();
+            if((mouseScroll = Input.GetAxis("Mouse ScrollWheel")) != 0){
+                updateVisionZoom();
+            }
         }
         
+    }
+
+    private void updateVisionPosition(){
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                visionCanvas.transform as RectTransform,
+                Input.mousePosition, visionCanvas.worldCamera,
+                out movePos);
+
+            transform.position = visionCanvas.transform.TransformPoint(movePos);
+    }
+
+    private void updateVisionZoom(){
+        newZoom = zoomCamera.fieldOfView - mouseScroll*zoomSpeed;
+        newZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
+
+        zoomCamera.fieldOfView = newZoom;  
     }
 }
