180d0336b74c4c9d1815398ed9d7169400f6754c
xiaodiode <xgloriaxiaox@gmail.com>
2023-12-24 21:01:34 -0600

    scene layers adjust correctly based on room size
    middle and front layers adjust width based on wallpaper width

--- Lucid/Assets/Scripts/Mansion/View.cs
+++ Lucid/Assets/Scripts/Mansion/View.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class View : MonoBehaviour
 {
     [SerializeField] private RectTransform wallpaper;
-    [SerializeField] private float wallpaperWidth;
+    [SerializeField] private float wallpaperWidth; //used for debugging
 
     [SerializeField] private List<Furniture> backFurniture = new();
     [SerializeField] private List<Furniture> midFurniture = new();
@@ -18,14 +18,25 @@ public class View : MonoBehaviour
     [SerializeField] private float yCameraPosition;
 
     private float backOffset, midOffset, frontOffset;
-
-    
+    Vector2 newMidOffset, newFrontOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         wallpaperWidth = wallpaper.rect.width;
-    }
+
+        midOffset = wallpaperWidth/3;
+        frontOffset = 2*(wallpaperWidth/3);
+
+        newMidOffset = middleLayer.offsetMax;
+        newFrontOffset = frontLayer.offsetMax;
+
+        newMidOffset.x = midOffset;
+        newFrontOffset.x = frontOffset;
+
+        middleLayer.offsetMax = newMidOffset;
+        frontLayer.offsetMax = newFrontOffset;
+    }   
 
     // Update is called once per frame
     void Update()
