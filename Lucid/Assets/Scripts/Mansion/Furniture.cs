30f085e916d5773700d4807b0d4de7c6d204471e
xiaodiode <xgloriaxiaox@gmail.com>
2023-12-24 23:42:32 -0600

    adjusting furniture to respective layers work
    after mid and top layers are adjusted according to room size, furniture is adjusted to their scaled positions on their respective layers and moved to that layer

--- Lucid/Assets/Scripts/Mansion/Furniture.cs
+++ Lucid/Assets/Scripts/Mansion/Furniture.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Furniture : MonoBehaviour
 {
+    [Tooltip("Scene Layer Options: bot, mid, top")]
+    [SerializeField] public string sceneLayer;
     // Start is called before the first frame update
     void Start()
     {
--- Lucid/Assets/Scripts/Mansion/View.cs
+++ Lucid/Assets/Scripts/Mansion/View.cs
@@ -6,41 +6,73 @@ public class View : MonoBehaviour
 {
     [SerializeField] private RectTransform wallpaper;
     [SerializeField] private float wallpaperWidth; //used for debugging
+    [SerializeField] private float midWidth;
+    [SerializeField] private float topWidth;
 
-    [SerializeField] private List<Furniture> backFurniture = new();
-    [SerializeField] private List<Furniture> midFurniture = new();
-    [SerializeField] private List<Furniture> frontFurniture = new();
+    [SerializeField] private List<RectTransform> botFurniture = new();
+    [SerializeField] private List<RectTransform> midFurniture = new();
+    [SerializeField] private List<RectTransform> topFurniture = new();
 
-    [SerializeField] private RectTransform backLayer;
-    [SerializeField] private RectTransform middleLayer;
-    [SerializeField] private RectTransform frontLayer;
+    [SerializeField] private RectTransform botLayer;
+    [SerializeField] private RectTransform midLayer;
+    [SerializeField] private RectTransform topLayer;
 
     [SerializeField] private float yCameraPosition;
 
-    private float backOffset, midOffset, frontOffset;
-    Vector2 newMidOffset, newFrontOffset;
+    private float botOffset, midOffset, topOffset;
+    Vector2 newMidOffset, newTopOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        wallpaperWidth = wallpaper.rect.width;
-
-        midOffset = wallpaperWidth/3;
-        frontOffset = 2*(wallpaperWidth/3);
-
-        newMidOffset = middleLayer.offsetMax;
-        newFrontOffset = frontLayer.offsetMax;
-
-        newMidOffset.x = midOffset;
-        newFrontOffset.x = frontOffset;
-
-        middleLayer.offsetMax = newMidOffset;
-        frontLayer.offsetMax = newFrontOffset;
+        adjustLayerWidths();
+        updateFurniturePos();
     }   
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void adjustLayerWidths(){
+        wallpaperWidth = wallpaper.rect.width;  
+
+        /* extend mid and top layer lengths based on
+        current room size */
+        midOffset = wallpaperWidth/3;
+        midWidth = wallpaperWidth + midOffset;
+
+        topOffset = 2*(wallpaperWidth/3);
+        topWidth = wallpaperWidth + topOffset;
+
+        newMidOffset = midLayer.offsetMax;
+        newTopOffset = topLayer.offsetMax;
+
+        newMidOffset.x = midWidth;
+        newTopOffset.x = topWidth;
+
+        midLayer.offsetMax = newMidOffset;
+        topLayer.offsetMax = newTopOffset;
+    }
+
+    private void updateFurniturePos(){
+        Vector2 position = new();
+        float oldX, newX;
+        foreach(RectTransform furniture in midFurniture){
+            position = furniture.anchoredPosition;
+            Debug.Log("furniture: " + furniture + ", furniture anchored position: " + position);
+            
+            // scale x position to new layer length
+            oldX = position.x;
+            newX = midWidth*(oldX / wallpaperWidth);    
+            Debug.Log("new X position: " + newX);
+
+            // update furniture position
+            position.x = newX;
+            furniture.anchoredPosition = position;
+
+            furniture.SetParent(midLayer);
+        }
     }
 }
